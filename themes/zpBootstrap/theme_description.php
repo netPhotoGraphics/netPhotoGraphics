bcb246b343238a044c5d588ed4c7730eacde2baf
stephen@sbillard.org <stephen@sbillard.org>
2018-10-04 10:57:59 -0700

    fixes to zoBootstrap update

--- themes/zpBootstrap/theme_description.php
+++ themes/zpBootstrap/theme_description.php
@@ -1,4 +1,5 @@
 <?php
+
 $me = basename(dirname(__FILE__));
 
 // Theme definition file
@@ -6,8 +7,7 @@ $theme_description['name'] = 'zpBootstrap';
 $theme_description['author'] = 'Vincent3569';
 $theme_description['version'] = '2.1';
 $theme_description['date'] = '21/09/2017';
-$theme_description['desc'] =
-gettext('A responsive theme ready for desktop to mobile device, using <a href="http://getbootstrap.com/" title="Boostrap" target="_blank">Boostrap 3.3.7</a> framework.', $me) . '<br />' .
-gettext('Please read', $me) . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/readme.htm' . '" title="Readme File" target="_blank">Readme</a>, <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/license.htm' . '" title="License File" target="_blank">License</a> ' . gettext('and', $me) . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/changelog.txt' . '" title="ChangeLog File" target="_blank">ChangeLog</a><br />' .
-gettext('To design your content to be responsive using Boostrap framework, you can have a look on', $me). ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/bootstrap_grid_system.htm' . '" title="Bootstrap Grid System" target="_blank">' . gettext('Bootstrap Grid System', $me) . '</a>';
+$theme_description['desc'] = gettext('A responsive theme ready for desktop to mobile device, using <a href="http://getbootstrap.com/" title="Boostrap" target="_blank">Boostrap 3.3.7</a> framework.') . '<br />' .
+				gettext('Please read') . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/readme.htm' . '" title="Readme File" target="_blank">Readme</a>, <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/license.htm' . '" title="License File" target="_blank">License</a> ' . gettext('and') . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/changelog.txt' . '" title="ChangeLog File" target="_blank">ChangeLog</a><br />' .
+				gettext('To design your content to be responsive using Boostrap framework, you can have a look on') . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/bootstrap_grid_system.htm' . '" title="Bootstrap Grid System" target="_blank">' . gettext('Bootstrap Grid System') . '</a>';
 ?>
--- themes/zpBootstrap/themeoptions.php
+++ themes/zpBootstrap/themeoptions.php
@@ -65,13 +65,13 @@ class ThemeOptions {
 						'order' => 0,
 						'key' => 'zpB_homepage',
 						'type' => OPTION_TYPE_CHECKBOX,
-						'desc' => gettext('Display a home page, with a slider of random pictures, the gallery description and the latest news.', $me)),
-				gettext('Latest news on Homepage', $me) => array(
+						'desc' => gettext('Display a home page, with a slider of random pictures, the gallery description and the latest news.')),
+				gettext('Latest news on Homepage') => array(
 						'order' => 1,
 						'key' => 'zpB_latest_news_homepage',
 						'type' => OPTION_TYPE_CHECKBOX,
-						'desc' => gettext('Display the latest news on the home page (Homepage option have to be selected too).', $me)),
-				gettext('Homepage slider', $me) => array(
+						'desc' => gettext('Display the latest news on the home page (Homepage option have to be selected too).')),
+				gettext('Homepage slider') => array(
 						'order' => 2,
 						'key' => 'zpB_homepage_album_filename',
 						'type' => OPTION_TYPE_SELECTOR,
@@ -79,15 +79,15 @@ class ThemeOptions {
 						'selections' => $albums,
 						'multilingual' => 0,
 						'desc' =>
-						gettext('Select the Album to use for the homepage slider (Dynamic albums may used).', $me) . '<br />' .
-						gettext('If Gallery is selected, the whole gallery will be used for the slider.', $me)),
-				gettext('Random pictures for homepage slider', $me) => array(
+						gettext('Select the Album to use for the homepage slider (Dynamic albums may used).') . '<br />' .
+						gettext('If Gallery is selected, the whole gallery will be used for the slider.')),
+				gettext('Random pictures for homepage slider') => array(
 						'order' => 4,
 						'key' => 'zpB_homepage_random_pictures',
 						'type' => OPTION_TYPE_TEXTBOX,
 						'multilingual' => 0,
-						'desc' => gettext('Number of random pictures to use for the homepage slider.', $me)),
-				gettext('Use infinite scroll', $me) => array(
+						'desc' => gettext('Number of random pictures to use for the homepage slider.')),
+				gettext('Use infinite scroll') => array(
 						'order' => 5,
 						'key' => 'zpB_infinitescroll',
 						'type' => OPTION_TYPE_CHECKBOX_ARRAY,
@@ -96,51 +96,51 @@ class ThemeOptions {
 								gettext('Albums') => 'zpB_use_infinitescroll_albums',
 								gettext('News') => 'zpB_use_infinitescroll_news'),
 						'desc' =>
-						gettext('Check pages which use <a href="https://infinite-scroll.com/" target="_blank">infinite-scroll jQuery plugin</a>. This layout will automatically load items of next page (albums, images or news) without pagination.', $me) . '<br />' .
-						gettext('The behavior is "manual first": it requires visitor to click a button the first time to load new items and then, it automatically load after.', $me) . '<br />' .
-						gettext('Rather than using infinite-scroll layout for all albums, you may also allow "multiple_layouts" plugin and then choose "album_infinitescroll" as layout for specific albums of your gallery.', $me) .
-						'<p class="notebox">' . gettext('<strong>Note:</strong> This album layout does not manage albums with images and sub-albums (in that case, standard album layout is automatically used).', $me) . '</p>'),
-				gettext('Use isotope', $me) => array(
+						gettext('Check pages which use <a href="https://infinite-scroll.com/" target="_blank">infinite-scroll jQuery plugin</a>. This layout will automatically load items of next page (albums, images or news) without pagination.') . '<br />' .
+						gettext('The behavior is "manual first": it requires visitor to click a button the first time to load new items and then, it automatically load after.') . '<br />' .
+						gettext('Rather than using infinite-scroll layout for all albums, you may also allow "multiple_layouts" plugin and then choose "album_infinitescroll" as layout for specific albums of your gallery.') .
+						'<p class="notebox">' . gettext('<strong>Note:</strong> This album layout does not manage albums with images and sub-albums (in that case, standard album layout is automatically used).') . '</p>'),
+				gettext('Use isotope') => array(
 						'order' => 6,
 						'key' => 'zpB_use_isotope',
 						'type' => OPTION_TYPE_CHECKBOX,
 						'desc' =>
-						gettext('Use <a href="https://isotope.metafizzy.co/" target="_blank">isotope jQuery plugin</a> for albums pages. This layout allows to display uncropped thumbnails and to filter them based on their tags.', $me) . '<br />' .
-						gettext('Rather than use isotope layout for all albums, you may also allow "multiple_layouts" plugin and then choice "album_isotope" as layout for specific albums of your gallery.', $me) .
+						gettext('Use <a href="https://isotope.metafizzy.co/" target="_blank">isotope jQuery plugin</a> for albums pages. This layout allows to display uncropped thumbnails and to filter them based on their tags.') . '<br />' .
+						gettext('Rather than use isotope layout for all albums, you may also allow "multiple_layouts" plugin and then choice "album_isotope" as layout for specific albums of your gallery.') .
 						'<p class="notebox">' .
-						gettext('<strong>Notes:</strong> This album layout does not manage sub-albums (in that case, only pictures of the album are shown and you cant not access on sub-albums).', $me) . '<br />' .
-						gettext('This option overwrites the infinite scroll on album option above.', $me) .
+						gettext('<strong>Notes:</strong> This album layout does not manage sub-albums (in that case, only pictures of the album are shown and you cant not access on sub-albums).') . '<br />' .
+						gettext('This option overwrites the infinite scroll on album option above.') .
 						'</p>'),
-				gettext('Social Links', $me) => array(
+				gettext('Social Links') => array(
 						'order' => 8,
 						'key' => 'zpB_social_links',
 						'type' => OPTION_TYPE_CHECKBOX,
-						'desc' => gettext('Check to show some social links.', $me)),
+						'desc' => gettext('Check to show some social links.')),
 				gettext('Allow search') => array(
 						'order' => 10,
 						'key' => 'zpB_allow_search',
 						'type' => OPTION_TYPE_CHECKBOX,
 						'desc' => gettext('Check to enable search form.')),
-				gettext('Excerpt length', $me) => array(
+				gettext('Excerpt length') => array(
 						'order' => 11,
 						'key' => 'zpB_exerpt_length',
 						'type' => OPTION_TYPE_TEXTBOX,
-						'desc' => gettext('Excerpt length for news and pages lists in search page', $me)),
+						'desc' => gettext('Excerpt length for news and pages lists in search page')),
 				gettext('Archive View') => array(
 						'order' => 12,
 						'key' => 'zpB_show_archive',
 						'type' => OPTION_TYPE_CHECKBOX,
-						'desc' => gettext('Display a link to the Archive list.', $me)),
+						'desc' => gettext('Display a link to the Archive list.')),
 				gettext('Tags') => array(
 						'order' => 14,
 						'key' => 'zpB_show_tags',
 						'type' => OPTION_TYPE_CHECKBOX,
-						'desc' => gettext('Check to show a tag cloud in Archive list, with all the tags of the gallery.', $me)),
+						'desc' => gettext('Check to show a tag cloud in Archive list, with all the tags of the gallery.')),
 				gettext('Exif') => array(
 						'order' => 16,
 						'key' => 'zpB_show_exif',
 						'type' => OPTION_TYPE_CHECKBOX,
-						'desc' => gettext('Show the EXIFs Data on Image page. Remember you have to check EXIFs data you want to show on Options>Image>Metadata.', $me))
+						'desc' => gettext('Show the EXIFs Data on Image page. Remember you have to check EXIFs data you want to show on Options>Image>Metadata.'))
 		);
 	}
 
