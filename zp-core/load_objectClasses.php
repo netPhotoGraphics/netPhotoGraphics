a3376437d3353f8376c02e6356241455db62baf7
stephen@sbillard.org <stephen@sbillard.org>
2018-11-23 10:42:21 -0800

    improve setup plugin load sequence

--- zp-core/load_objectClasses.php
+++ zp-core/load_objectClasses.php
@@ -24,10 +24,12 @@ if (DEBUG_PLUGINS) {
 }
 if (abs(OFFSET_PATH) == 2) {
 	// setup does not need (and might have problems with) plugins so just load some specific ones
-	$enabled = array();
+	//	NOTE: these should be ordered by priority, descending
+	$enabled = array(
+			'dynamic-locale' => array('priority' => 10 | CLASS_PLUGIN, 'path' => SERVERPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/dynamic-locale.php')
+	);
 	if (extensionEnabled('googleTFA')) {
 		$enabled['googleTFA'] = array('priority' => 5 | CLASS_PLUGIN, 'path' => SERVERPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/googleTFA.php');
-		$enabled['dynamic-locale'] = array('priority' => 10 | CLASS_PLUGIN, 'path' => SERVERPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/dynamic-locale.php');
 	}
 } else {
 	$enabled = getEnabledPlugins();
