f17a6c0e29a4cd930d962ceca12120eff0ba8999
stephen@sbillard.org <stephen@sbillard.org>
2019-02-27 15:22:41 -0800

    release build 1.9.1.0.DEV_05

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,4 +1,4 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('ZENPHOTO_VERSION', '1.9.1.0.DEV_04');
+define('ZENPHOTO_VERSION', '1.9.1.0.DEV_05');
 ?>
