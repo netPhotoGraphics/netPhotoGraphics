6a347aab9492fda57eba4a0a50e6a926fa0b12be
stephen@sbillard.org <stephen@sbillard.org>
2019-03-06 16:04:51 -0800

    did not save the correct change

--- zp-core/zp-extensions/uploader_jQuery/js/main.js
+++ zp-core/zp-extensions/uploader_jQuery/js/main.js
@@ -18,7 +18,7 @@ $(function () {
 	$('#fileupload').fileupload({
 		// Uncomment the following to send cross-domain cookies:
 		//xhrFields: {withCredentials: true},
-		url: '/dev/zp-core/zp-extensions/uploader_jQuery/server/php/index.php'
+		url: '../zp-extensions/uploader_jQuery/server/php/index.php'
 	});
 
 	// Enable iframe cross-domain access via redirect option:
