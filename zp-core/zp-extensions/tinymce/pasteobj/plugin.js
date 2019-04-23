dae6fabdf7e21e84600731205b9c1071865adc21
stephen@sbillard.org <stephen@sbillard.org>
2019-04-23 11:07:59 -0700

    TinyMCE version 5.0.4 which supports urldialogs and update pasteObj to use same

--- zp-core/zp-extensions/tinymce/pasteobj/plugin.js
+++ zp-core/zp-extensions/tinymce/pasteobj/plugin.js
@@ -9,36 +9,12 @@
 
 tinymce.PluginManager.add('pasteobj', function (editor, url) {
 
-	function _onAction(editor, url) {
-		// Open window with a specific url
-		var windowManagerURL = url.replace('/plugins/pasteobj', '/pasteobj/pasteobj.php'),
-						windowManagerCSS = '<style type="text/css">' +
-						'.tox-dialog {max-width: 100%!important; width:800px!important; overflow: hidden; height:600px!important; bborder-radius:0.25em;}' +
-						'.tox-dialog__header{ border-bottom: 1px solid lightgray!important; }' + // for custom header in filemanage
-						'.tox-dialog__footer { display: none!important; }' + // for custom footer in filemanage
-						'.tox-dialog__body { padding: 5!important; }' +
-						'.tox-dialog__body-content > div { height: 100%; overflow:hidden}' +
-						'</style > ';
-
-		editor.windowManager.open({
-			title: 'netPhotoGraphics:obj',
-			body: {
-				type: 'panel',
-				items: [{
-						type: 'htmlpanel',
-						html: windowManagerCSS + '<iframe src="' + windowManagerURL + '"  frameborder="0" style="width:100%; height:100%"></iframe>'
-					}]
-			},
-			buttons: []
-		});
-	}
-
 	// Add a button that opens a window
 	editor.ui.registry.addButton('pasteobj', {
 		icon: "paste",
 		tooltip: "netPhotoGraphics:obj",
 		onAction: function () {
-			_onAction(editor, url);
+			editor.windowManager.openUrl(pasteObjConfig);
 		}
 	});
 	// Adds a menu item to the tools menu
@@ -46,7 +22,7 @@ tinymce.PluginManager.add('pasteobj', function (editor, url) {
 		icon: "paste",
 		text: 'netPhotoGraphics:obj...',
 		onAction: function () {
-			_onAction(editor, url);
+			editor.windowManager.openUrl(pasteObjConfig);
 		}
 	});
 
