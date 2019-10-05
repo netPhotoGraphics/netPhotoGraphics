3553d6b298fc5b3ac53ebf5c6b93175d1ff1b6a6
stephen@sbillard.org <stephen@sbillard.org>
2019-10-05 14:34:17 -0700

    release build 2.00.02.07.09

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,4 +1,4 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('NETPHOTOGRAPHICS_VERSION', '2.00.02.07.08');
+define('NETPHOTOGRAPHICS_VERSION', '2.00.02.07.09');
 ?>
