12f8cba50b16e72379e7be51fcee5793b405290e
stephen@sbillard.org <stephen@sbillard.org>
2020-03-05 11:19:19 -0800

    Reference actual image filename for missing image message

--- npgCore/i.php
+++ npgCore/i.php
@@ -125,7 +125,7 @@ if (!file_exists($imgfile)) {
 		if (DEBUG_IMAGE) {
 			debugLogVar(['image not found' => $args]);
 		}
-		imageProcessing::error('404 Not Found', sprintf(gettext("Image not found; file %s does not exist."), html_encode(filesystemToInternal($album . '/' . $image))), 'err-imagenotfound.png');
+		imageProcessing::error('404 Not Found', sprintf(gettext("Image not found; file %s does not exist."), html_encode(filesystemToInternal($imgfile))), 'err-imagenotfound.png');
 	}
 }
 
