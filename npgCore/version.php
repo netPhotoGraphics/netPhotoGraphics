b1e8c2500a04f02514ce38c80da14d512e3b42d7
stephen@sbillard.org <stephen@sbillard.org>
2020-09-24 16:44:19 -0700

    release build 2.00.07.01.04

--- npgCore/version.php
+++ npgCore/version.php
@@ -1,4 +1,4 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('NETPHOTOGRAPHICS_VERSION', '2.00.07.01.03');
+define('NETPHOTOGRAPHICS_VERSION', '2.00.07.01.04');
 ?>
