f7418c6d24dda7e5150190c77cfc694fafd3ad39
Stephen <stephen@sbillard.org>
2021-03-01 13:15:28 -0800

    fix check on not valid image

--- npgCore/class-image.php
+++ npgCore/class-image.php
@@ -177,7 +177,7 @@ class Image extends MediaObject {
 		$msg = false;
 		if (!is_object($album) || !$album->exists) {
 			$msg = sprintf(gettext('Invalid %s instantiation: Album does not exist'), get_class($this)) . ' (' . $album . ')';
-		} else if (!$this->classSetup($album, $filename) || !file_exists($this->localpath) || is_dir($this->localpath)) {
+		} else if (!file_exists($album->localpath . $filename) || is_dir($album->localpath . $filename) || !$this->classSetup($album, $filename)) {
 			$msg = sprintf(gettext('Invalid %s instantiation: file does not exist'), get_class($this)) . ' (' . $album . '/' . $filename . ')';
 		}
 		return $msg;
