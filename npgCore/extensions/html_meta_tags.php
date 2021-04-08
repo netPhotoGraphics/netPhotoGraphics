17d9d646e0a941ad40f948279afa3df8ca5675fe
Stephen <stephen@sbillard.org>
2021-04-08 09:50:39 -0700

    add missing sources

--- npgCore/extensions/html_meta_tags.php
+++ npgCore/extensions/html_meta_tags.php
@@ -225,14 +225,20 @@ class htmlmetatags {
 					$twittercard_type = 'summary_large_image';
 				}
 				$metadata = $_current_image->getMetaData();
-				if (isset($metadata['EXIFArtistt']) && !empty($metadata['EXIFArtist'])) {
+				if (isset($metadata['XMPImageCredit']) && !empty($metadata['XMPImageCredit'])) {
+					$author = $metadata['XMPImageCredit'];
+				} else if (isset($metadata['EXIFArtist']) && !empty($metadata['EXIFArtist'])) {
 					$author = $metadata['EXIFArtist'];
+				} else if (isset($metadata['IPTCByLine']) && !empty($metadata['IPTCByLine'])) {
+					$author = $metadata['IPTCByLine'];
 				} else {
 					if ($holder = self::getOwnerName($_current_image->getOwner())) {
 						$author = $holder;
 					}
 				}
-				if (isset($metadata['IPTCCopyright']) && !empty($metadata['IPTCCopyright'])) {
+				if (isset($metadata['XMPCopyright']) && !empty($metadata['XMPCopyright'])) {
+					$copyright_notice = $metadata['XMPCopyright'];
+				} else if (isset($metadata['IPTCCopyright']) && !empty($metadata['IPTCCopyright'])) {
 					$copyright_notice = $metadata['IPTCCopyright'];
 				} else if (isset($metadata['EXIFCopyright']) && !empty($metadata['EXIFCopyright'])) {
 					$copyright_notice = $metadata['EXIFCopyright'];
