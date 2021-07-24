ec46d1e8441a8b0d461dc7373f589ecd415d4d82
Stephen <stephen@sbillard.org>
2021-07-24 12:30:43 -0700

    safe handling of corrupted databases.

--- npgCore/class-gallery.php
+++ npgCore/class-gallery.php
@@ -663,43 +663,45 @@ class Gallery {
 					while ($analbum = db_fetch_assoc($albumids)) {
 						if (($mtime = filemtime(ALBUM_FOLDER_SERVERPATH . internalToFilesystem($analbum['folder']))) > $analbum['mtime']) {
 							// refresh
-							$album = newAlbum($analbum['folder']);
-							$album->set('mtime', $mtime);
-							if ($this->getAlbumUseImagedate()) {
-								$album->setDateTime(NULL);
-							}
-							if ($album->isDynamic()) {
-								$data = file_get_contents($album->localpath);
-								$thumb = getOption('AlbumThumbSelect');
-								$words = $fields = '';
-								while (!empty($data)) {
-									$data1 = trim(substr($data, 0, $i = strpos($data, "\n")));
-									if ($i === false) {
-										$data1 = $data;
-										$data = '';
-									} else {
-										$data = substr($data, $i + 1);
-									}
-									if (strpos($data1, 'WORDS=') !== false) {
-										$words = "words=" . urlencode(substr($data1, 6));
-									}
-									if (strpos($data1, 'THUMB=') !== false) {
-										$thumb = trim(substr($data1, 6));
-									}
-									if (strpos($data1, 'FIELDS=') !== false) {
-										$fields = "&searchfields=" . trim(substr($data1, 7));
-									}
+							$album = newAlbum($analbum['folder'], false, true);
+							if ($album->exists) { //	Guard against instantiation failure
+								$album->set('mtime', $mtime);
+								if ($this->getAlbumUseImagedate()) {
+									$album->setDateTime(NULL);
 								}
-								if (!empty($words)) {
-									if (empty($fields)) {
-										$fields = '&searchfields=tags';
+								if ($album->isDynamic()) {
+									$data = file_get_contents($album->localpath);
+									$thumb = getOption('AlbumThumbSelect');
+									$words = $fields = '';
+									while (!empty($data)) {
+										$data1 = trim(substr($data, 0, $i = strpos($data, "\n")));
+										if ($i === false) {
+											$data1 = $data;
+											$data = '';
+										} else {
+											$data = substr($data, $i + 1);
+										}
+										if (strpos($data1, 'WORDS=') !== false) {
+											$words = "words=" . urlencode(substr($data1, 6));
+										}
+										if (strpos($data1, 'THUMB=') !== false) {
+											$thumb = trim(substr($data1, 6));
+										}
+										if (strpos($data1, 'FIELDS=') !== false) {
+											$fields = "&searchfields=" . trim(substr($data1, 7));
+										}
 									}
+									if (!empty($words)) {
+										if (empty($fields)) {
+											$fields = '&searchfields=tags';
+										}
+									}
+									$album->set('search_params', $words . $fields);
+									$album->set('thumb', $thumb);
 								}
-								$album->set('search_params', $words . $fields);
-								$album->set('thumb', $thumb);
+								$album->save();
+								npgFilters::apply('album_refresh', $album);
 							}
-							$album->save();
-							npgFilters::apply('album_refresh', $album);
 						}
 					}
 					db_free_result($albumids);
@@ -914,14 +916,16 @@ class Gallery {
 		//	albums are now in the correct order
 		$albums_ordered = array();
 		foreach ($results as $folder => $row) { // check for visible
-			$album = newAlbum($folder);
-			$subrights = $album->subrights();
-			if ($mine ||
-							($album->getShow() || $viewUnpublished) // published or overridden by parameter
-							|| $subrights && is_null($album->getParent()) // is the user's managed album
-							|| $subrights && ($subrights & MANAGED_OBJECT_RIGHTS_VIEW ) //	managed subalbum and user has unpublished rights
-			) {
-				$albums_ordered[] = $folder;
+			$album = newAlbum($folder, true, true);
+			if ($album->exists) { // fail to instantiate?
+				$subrights = $album->subrights();
+				if ($mine ||
+								($album->getShow() || $viewUnpublished) // published or overridden by parameter
+								|| $subrights && is_null($album->getParent()) // is the user's managed album
+								|| $subrights && ($subrights & MANAGED_OBJECT_RIGHTS_VIEW ) //	managed subalbum and user has unpublished rights
+				) {
+					$albums_ordered[] = $folder;
+				}
 			}
 		}
 		return $albums_ordered;
