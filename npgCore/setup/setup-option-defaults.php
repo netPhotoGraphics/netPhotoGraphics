98dcc49bd78c505756b13fd2e75d611d25161f57
Stephen <stephen@sbillard.org>
2022-05-02 11:30:42 -0700

    fully translate (if possible) strftime format strings to DateTime format strings

--- npgCore/setup/setup-option-defaults.php
+++ npgCore/setup/setup-option-defaults.php
@@ -686,50 +686,59 @@ setOptionDefault('protect_full_image', 'Protected');
 
 setOptionDefault('locale', '');
 
-//	update old date formats to be compatible with PHP 8.0 date formatting
-$formatXlate = array(
-		'%d/%m/%y %H:%M' => 'd/m/y H:i',
-		'%d/%m/%y' => 'd/m/y',
-		'%d/%m/%Y %H:%M' => 'd/m/Y H:i',
-		'%d/%m/%Y' => 'd/m/Y',
-		'%d-%m-%y %H:%M' => 'd-m-y H:i',
-		'%d-%m-%y' => 'd-m-y',
-		'%d-%m-%Y %H:%M' => 'd-m-Y H:i',
-		'%d-%m-%Y' => 'd-m-Y',
-		'%Y. %B %d. %H:%M' => 'Y. F d. H:i',
-		'%Y. %B %d.' => 'Y. F d.',
-		'%Y-%m-%d %H:%M' => 'Y-m-d H:i',
-		'%Y-%m-%d' => 'Y-m-d',
-		'%d %B %Y %H:%M' => 'd F Y H:i',
-		'%d %B %Y' => 'd F Y',
-		'%d %B %Y %H:%M' => 'd F Y H:i',
-		'%d %B %Y' => 'd F Y',
-		'%d. %B %Y %H:%M' => 'd. F Y H:i',
-		'%d. %B %Y' => 'd. F Y',
-		'%d. %b %y %H:%M' => 'd. M y H:i',
-		'%d. %b %y' => 'd. M y',
-		'%d. %B %Y %H:%M' => 'd. F Y H:i',
-		'%d. %B %Y' => 'd. F Y',
-		'%d.%m.%y %H:%M' => 'd.m.y H:i',
-		'%d.%m.%y' => 'd.m.y',
-		'%d.%m.%Y %H:%M' => 'd.m.Y H:i',
-		'%d.%m.%Y' => 'd.m.Y',
-		'%d-%m-%y %H:%M' => 'd-m-y H:i',
-		'%d-%m-%y' => 'd-m-y',
-		'%d-%m-%Y %H:%M' => 'd-m-Y H:i',
-		'%d-%m-%Y' => 'd-m-Y',
-		'%d-%b-%y %H:%M' => 'd-M-y H:i',
-		'%d-%b-%y' => 'd-M-y',
-		'%d-%b-%Y %H:%M' => 'd-M-Y H:i',
-		'%d-%b-%Y' => 'd-M-Y',
-		'%b %d, %Y %H:%M' => 'M d, Y H:i',
-		'%b %d, %Y' => 'M d, Y',
-		'%B %d, %Y %H:%M' => 'F d, Y H:i',
-		'%B %d, %Y' => 'F d, Y'
+//	update old strftime date formats to be compatible with DateTime formatting as strftime is deprecated in PHP 8.0
+$strftimeXlate = array(
+		'%a' => 'D', // An abbreviated textual representation of the day	Sun through Sat
+		'%A' => 'l', // A full textual representation of the day	Sunday through Saturday
+		'%d' => 'd', // Two-digit day of the month (with leading zeros)	01 to 31
+		'%e' => 'j', // Day of the month, with a space preceding single digits. Not implemented as described on Windows. See below for more information.	1 to 31
+		'%j' => 'z', // Day of the year, 3 digits with leading zeros	001 to 366
+		'%u' => 'N', // ISO-8601 numeric representation of the day of the week	1 (for Monday) through 7 (for Sunday)
+		'%w' => 'w', // Numeric representation of the day of the week	0 (for Sunday) through 6 (for Saturday)
+		//Week	---	---
+		'%U' => 'W', // Week number of the given year, starting with the first Sunday as the first week	13 (for the 13th full week of the year)
+		'%V' => 'W', // [sic]ISO-8601:1988 week number of the given year, starting with the first week of the year with at least 4 weekdays, with Monday being the start of the week	01 through 53 (where 53 accounts for an overlapping week)
+		'%W' => 'W', // [sic]A numeric representation of the week of the year, starting with the first Monday as the first week	46 (for the 46th week of the year beginning with a Monday)
+		//Month	---	---
+		'%b' => 'M', // Abbreviated month name, based on the locale	Jan through Dec
+		'%B' => 'F', // Full month name, based on the locale	January through December
+		'%h' => 'M', // Abbreviated month name, based on the locale (an alias of %b)	Jan through Dec
+		'%m' => 'm', // Two digit representation of the month	01 (for January) through 12 (for December)
+		//Year	---	---
+		'%C' => 'Y', // [sic]Two digit representation of the century (year divided by 100, truncated to an integer)	19 for the 20th Century
+		'%g' => 'o', // Two digit representation of the year going by ISO-8601:1988 standards (see %V)	Example: 09 for the week of January 6, 2009
+		'%G' => 'Y', // [sic]The full four-digit version of %g	Example: 2008 for the week of January 3, 2009
+		'%y' => 'y', // Two digit representation of the year	Example: 09 for 2009, 79 for 1979
+		'%Y' => 'Y', // Four digit representation for the year	Example: 2038
+		//Time------
+		'%H' => 'H', // Two digit representation of the hour in 24-hour format	00 through 23
+		'%k' => 'G', // Hour in 24-hour format, with a space preceding single digits	0 through 23
+		'%I' => 'h', // Two digit representation of the hour in 12-hour format	01 through 12
+		'%l' => 'h', // [sic](lower-case 'L')	Hour in 12-hour format, with a space preceding single digits	1 through 12
+		'%M' => 'i', // Two digit representation of the minute	00 through 59
+		'%p' => 'A', // UPPER-CASE 'AM' or 'PM' based on the given time	Example: AM for 00:31, PM for 22:23
+		'%P' => 'a', // lower-case 'am' or 'pm' based on the given time	Example: am for 00:31, pm for 22:23
+		'%r' => 'h:i:s', // Same as "%I:%M:%S %p"	Example: 09:34:17 PM for 21:34:17
+		'%R' => 'H:i', // Same as "%H:%M"	Example: 00:35 for 12:35 AM, 16:44 for 4:44 PM
+		'%S' => 's', // Two digit representation of the second	00 through 59
+		'%T' => 'H:i:s', // Same as "%H:%M:%S"	Example: 21:34:17 for 09:34:17 PM
+		'%X' => '%X', // Preferred time representation based on locale, without the date	Example: 03:59:16 or 15:59:16
+		'%z' => 'O', // The time zone offset. Not implemented as described on Windows. See below for more information.	Example: -0500 for US Eastern Time
+		'%Z' => 'T', // The time zone abbreviation. Not implemented as described on Windows. See below for more information.	Example: EST for Eastern Time
+		//Time and Date Stamps	---	---
+		'%c' => '%x', // Preferred date and time stamp based on locale	Example: Tue Feb 5 00:45:10 2009 for February 5, 2009 at 12:45:10 AM
+		'%D' => 'm/d/y', // Same as "%m/%d/%y"	Example: 02/05/09 for February 5, 2009
+		'%F' => 'y-m-d', // Same as "%Y-%m-%d" (commonly used in database datestamps)	Example: 2009-02-05 for February 5, 2009
+		'%s' => 'u', // Unix Epoch Time timestamp (same as the time() function)	Example: 305815200 for September 10, 1979 08:40:00 AM
+		'%x' => '%x', // Preferred date representation based on locale, without the time	Example: 02/05/09 for February 5, 2009
+		//Miscellaneous	---	---
+		'%n' => "\n", //	A newline character ("\n")	---
+		'%t' => "\t"		 // A Tab character ("\t")	---
 );
 $old = getOption('date_format');
-if (isset($formatXlate[$old])) {
-	setOption('date_format', $formatXlate[$old]);
+$new = strtr($old, $strftimeXlate);
+if ($old != $new) {
+	setOption('date_format', $new);
 }
 setOptionDefault('date_format', '%x');
 
