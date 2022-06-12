73f5cd0451b0d0db9c6d5ba6e308660a87df5318
Stephen <stephen@sbillard.org>
2022-06-11 18:11:11 -0700

    TinyMCE 5.10.5

--- npgCore/extensions/tinymce/config/config.js.php
+++ npgCore/extensions/tinymce/config/config.js.php
@@ -51,7 +51,7 @@ if ($MCEdirection == NULL) {
 	}
 }
 
-scriptLoader(TINYMCE . '/tinymce.5.8.0.min.js');
+scriptLoader(TINYMCE . '/tinymce.5.10.5.min.js');
 scriptLoader(TINYMCE . '/jquery.tinymce.min.js');
 if (OFFSET_PATH && getOption('dirtyform_enable') > 1) {
 	scriptLoader(CORE_SERVERPATH . 'js/dirtyforms/jquery.dirtyforms.helpers.tinymce.min.js');
