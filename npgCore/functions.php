53ba4070878da3f9835d9de9625933064d5299b9
Stephen <stephen@sbillard.org>
2023-04-27 17:29:19 -0700

    fix some format conversion mistakes

--- npgCore/functions.php
+++ npgCore/functions.php
@@ -440,8 +440,8 @@ function formattedDate($format, $dt = NULL) {
 				//	time zone
 				'e' => 'vv', //	Timezone identifier
 				'O' => 'xx', //	Difference to Greenwich time (GMT) without colon between hours and minutes
-				'P' => 'Z', //	Difference to Greenwich time (GMT) with colon between hours and minutes
-				'p' => 'XX', //	The same as P, but returns Z instead of +00:00 (available as of PHP 8.0.0)
+				'P' => 'xxx', //	Difference to Greenwich time (GMT) with colon between hours and minutes
+				'p' => 'XXX', //	The same as P, but returns Z instead of +00:00 (available as of PHP 8.0.0)
 				'T' => 'z', //	Timezone abbreviation, if known; otherwise the GMT offset.
 				// week
 				'N' => 'e', //	ISO 8601 numeric representation of the day of the week
