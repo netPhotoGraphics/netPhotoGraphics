ef527192a212d48e061738486f03404660ac8fda
Stephen <stephen@sbillard.org>
2023-07-01 13:22:53 -0700

    Allow NULL subalbum to imply gallery

--- npgCore/class-album.php
+++ npgCore/class-album.php
@@ -51,8 +51,16 @@ function isAlbumClass($album) {
  * @return array
  */
 function getNestedAlbumList($subalbum, $maxlevel, $level = array()) {
+	global $_gallery;
+	if (!is_array($level)) {
+		$level = array();
+	}
 	$cur = count($level);
-	$albums = $subalbum->getAlbums();
+	if (is_null($subalbum)) {
+		$albums = $_gallery->getAlbums();
+	} else {
+		$albums = $subalbum->getAlbums();
+	}
 
 	$list = array();
 	foreach ($albums as $analbum) {
