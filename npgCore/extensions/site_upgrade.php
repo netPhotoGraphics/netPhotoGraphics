8bc91dcc1a9b5ba13e8a296b2b2c3a478666c4d4
Stephen <stephen@sbillard.org>
2023-10-29 17:51:45 -0700

    some plugins need it loaded during setup

--- npgCore/extensions/site_upgrade.php
+++ npgCore/extensions/site_upgrade.php
@@ -41,275 +41,274 @@ $plugin_disable = npgFunctions::hasPrimaryScripts() ? false : gettext('Open and 
 
 if ($plugin_disable) {
 	enableExtension('site_upgrade', 0);
-} else {
-	define('SITE_UPGRADE_FILELIST', array(
-			'closed.htm' => '+', // copy and update define
-			'closed.php' => '*' // copy and update
-					)
-	);
+}
+define('SITE_UPGRADE_FILELIST', array(
+		'closed.htm' => '+', // copy and update define
+		'closed.php' => '*' // copy and update
+				)
+);
 
-	class site_upgrade {
+class site_upgrade {
 
-		static function notice() {
-			?>
-			<div style="width: 100%; position: fixed; top: 0px; left: 0px; z-index: 1000;" >
-				<p style="text-align: center;">
-					<strong style="background-color: #FFEFB7; color:black; padding: 5px;">
-						<?php echo gettext('Site is available for testing only.'); ?>
-					</strong>
+	static function notice() {
+		?>
+		<div style="width: 100%; position: fixed; top: 0px; left: 0px; z-index: 1000;" >
+			<p style="text-align: center;">
+				<strong style="background-color: #FFEFB7; color:black; padding: 5px;">
+					<?php echo gettext('Site is available for testing only.'); ?>
+				</strong>
+			</p>
+		</div>
+		<?php
+	}
+
+	static function note($where) {
+		global $_conf_vars;
+		switch (isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL) {
+			case 'closed':
+				if ($where == 'Overview') {
+					?>
+					<form class="dirtylistening" name="site_upgrade_form" id="site_upgrade_form">
+					</form>
+					<script type="text/javascript">
+						window.addEventListener('load', function () {
+							$('#site_upgrade_form').dirtyForms('setDirty');
+							$.DirtyForms.message = '<?php echo gettext('The site is closed!'); ?>';
+						}, false);
+					</script>
+					<?php
+				}
+				?>
+				<p class="errorbox">
+					<strong><?php echo gettext('The site is closed!'); ?></strong>
 				</p>
-			</div>
-			<?php
+				<?php
+				break;
+			case 'closed_for_test';
+				?>
+				<p class="notebox">
+					<strong><?php echo gettext('Site is available for testing only.'); ?></strong>
+				</p>
+				<?php
+				break;
 		}
+	}
 
-		static function note($where) {
-			global $_conf_vars;
-			switch (isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL) {
-				case 'closed':
-					if ($where == 'Overview') {
-						?>
-						<form class="dirtylistening" name="site_upgrade_form" id="site_upgrade_form">
-						</form>
-						<script type="text/javascript">
-							window.addEventListener('load', function () {
-								$('#site_upgrade_form').dirtyForms('setDirty');
-								$.DirtyForms.message = '<?php echo gettext('The site is closed!'); ?>';
-							}, false);
-						</script>
-						<?php
-					}
-					?>
-					<p class="errorbox">
-						<strong><?php echo gettext('The site is closed!'); ?></strong>
-					</p>
-					<?php
-					break;
-				case 'closed_for_test';
-					?>
-					<p class="notebox">
-						<strong><?php echo gettext('Site is available for testing only.'); ?></strong>
-					</p>
-					<?php
-					break;
-			}
+	static function status() {
+		global $_conf_vars;
+		switch (isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL) {
+			case 'closed':
+				?>
+				<li>
+					<?php echo gettext('Site status:'); ?> <span style="color:RED"><strong><?php echo gettext('The site is closed!'); ?></strong></span>
+				</li>
+				<?php
+				break;
+			case 'closed_for_test';
+				?>
+				<li>
+					<?php echo gettext('Site status:'); ?> <span style="color:RED"><strong><?php echo gettext('The site is in test mode!'); ?></strong></span>
+				</li>
+				<?php
+				break;
+			default:
+				?>
+				<li>
+					<?php echo gettext('Site status:'); ?> <strong><?php echo gettext('The site is opened'); ?></strong>
+				</li>
+				<?php
+				break;
 		}
+	}
 
-		static function status() {
-			global $_conf_vars;
-			switch (isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL) {
-				case 'closed':
-					?>
-					<li>
-						<?php echo gettext('Site status:'); ?> <span style="color:RED"><strong><?php echo gettext('The site is closed!'); ?></strong></span>
-					</li>
-					<?php
-					break;
-				case 'closed_for_test';
-					?>
-					<li>
-						<?php echo gettext('Site status:'); ?> <span style="color:RED"><strong><?php echo gettext('The site is in test mode!'); ?></strong></span>
-					</li>
-					<?php
-					break;
-				default:
-					?>
-					<li>
-						<?php echo gettext('Site status:'); ?> <strong><?php echo gettext('The site is opened'); ?></strong>
-					</li>
-					<?php
-					break;
-			}
-		}
+	static function button($buttons) {
+		global $_conf_vars;
+		$state = isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL;
 
-		static function button($buttons) {
-			global $_conf_vars;
-			$state = isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL;
+		$update = false;
+		$hash = getSerializedArray(getOption('site_upgrade_hash'));
 
-			$update = false;
-			$hash = getSerializedArray(getOption('site_upgrade_hash'));
-
-			foreach (npgFilters::apply('site_upgrade_xml', SITE_UPGRADE_FILELIST) as $name => $source) {
-				if (file_exists(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name)) {
-					if (!isset($hash[$name]) || $hash[$name] != md5(file_get_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name))) {
-						$update = true;
-						break;
-					}
-				} else {
+		foreach (npgFilters::apply('site_upgrade_xml', SITE_UPGRADE_FILELIST) as $name => $source) {
+			if (file_exists(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name)) {
+				if (!isset($hash[$name]) || $hash[$name] != md5(file_get_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name))) {
 					$update = true;
 					break;
 				}
+			} else {
+				$update = true;
+				break;
 			}
+		}
 
-			if ($update) {
+		if ($update) {
+			$buttons[] = array(
+					'XSRFTag' => 'site_upgrade_refresh',
+					'category' => gettext('Admin'),
+					'enable' => true,
+					'button_text' => gettext('Restore site_upgrade files'),
+					'formname' => 'refreshHTML',
+					'action' => getAdminLink('admin.php') . '?refreshHTML=1',
+					'icon' => CLOCKWISE_OPEN_CIRCLE_ARROW_GREEN,
+					'title' => gettext('Restores the files in the "plugins/site_upgrade" folder to their default state. Note: this will overwrite any custom edits you may have made.'),
+					'alt' => '',
+					'rights' => ADMIN_RIGHTS
+			);
+		}
+		switch ($state) {
+			case 'closed':
 				$buttons[] = array(
-						'XSRFTag' => 'site_upgrade_refresh',
+						'XSRFTag' => 'site_upgrade',
 						'category' => gettext('Admin'),
-						'enable' => true,
-						'button_text' => gettext('Restore site_upgrade files'),
-						'formname' => 'refreshHTML',
-						'action' => getAdminLink('admin.php') . '?refreshHTML=1',
-						'icon' => CLOCKWISE_OPEN_CIRCLE_ARROW_GREEN,
-						'title' => gettext('Restores the files in the "plugins/site_upgrade" folder to their default state. Note: this will overwrite any custom edits you may have made.'),
+						'enable' => 3,
+						'button_text' => gettext('Site » test mode'),
+						'formname' => 'site_upgrade',
+						'action' => getAdminLink(PLUGIN_FOLDER . '/site_upgrade/site_upgrade.php'),
+						'icon' => LOCK_OPEN,
+						'title' => gettext('Make the site available for viewing administrators only.'),
+						'onclick' => "$('#site_upgrade_form').dirtyForms('setClean');this.form.submit();",
 						'alt' => '',
+						'hidden' => '<input type="hidden" name="siteState" value="closed_for_test" />',
 						'rights' => ADMIN_RIGHTS
 				);
-			}
-			switch ($state) {
-				case 'closed':
-					$buttons[] = array(
-							'XSRFTag' => 'site_upgrade',
-							'category' => gettext('Admin'),
-							'enable' => 3,
-							'button_text' => gettext('Site » test mode'),
-							'formname' => 'site_upgrade',
-							'action' => getAdminLink(PLUGIN_FOLDER . '/site_upgrade/site_upgrade.php'),
-							'icon' => LOCK_OPEN,
-							'title' => gettext('Make the site available for viewing administrators only.'),
-							'onclick' => "$('#site_upgrade_form').dirtyForms('setClean');this.form.submit();",
-							'alt' => '',
-							'hidden' => '<input type="hidden" name="siteState" value="closed_for_test" />',
-							'rights' => ADMIN_RIGHTS
+				break;
+			case 'closed_for_test':
+				$buttons[] = array(
+						'XSRFTag' => 'site_upgrade',
+						'category' => gettext('Admin'),
+						'enable' => 2,
+						'button_text' => gettext('Site » open'),
+						'formname' => 'site_upgrade',
+						'action' => getAdminLink(PLUGIN_FOLDER . '/site_upgrade/site_upgrade.php'),
+						'icon' => LOCK_OPEN,
+						'title' => gettext('Make site available for viewing.'),
+						'alt' => '',
+						'hidden' => '<input type="hidden" name="siteState" value="open" />',
+						'rights' => ADMIN_RIGHTS
+				);
+				list($diff, $needs) = checkSignature(0);
+				if (empty($needs)) {
+					?>
+					<script type="text/javascript">
+						window.addEventListener('load', function () {
+							$('#site_upgrade').submit(function () {
+								return confirm('<?php echo gettext('Your setup scripts are not protected!'); ?>');
+							})
+						}, false);
+					</script>
+					<?php
+				}
+				break;
+			default:
+				$buttons[] = array(
+						'XSRFTag' => 'site_upgrade',
+						'category' => gettext('Admin'),
+						'enable' => true,
+						'button_text' => gettext('Site » close'),
+						'formname' => 'site_upgrade.php',
+						'action' => getAdminLink(PLUGIN_FOLDER . '/site_upgrade/site_upgrade.php'),
+						'icon' => LOCK,
+						'title' => gettext('Make site unavailable for viewing by redirecting to the "closed.html" page.'),
+						'alt' => '',
+						'hidden' => '<input type="hidden" name="siteState" value="closed" />',
+						'rights' => ADMIN_RIGHTS
+				);
+				break;
+		}
+
+		return $buttons;
+	}
+
+	static function updateXML($files) {
+		global $_gallery;
+		mkdir_recursive(USER_PLUGIN_SERVERPATH . 'site_upgrade/', FOLDER_MOD);
+		$hash = array();
+		foreach ($files as $name => $source) {
+			switch ($source) {
+				case '*':
+					$data = file_get_contents(PLUGIN_SERVERPATH . 'site_upgrade/' . $name);
+					$defines = array(
+							'SITEINDEX' => "/index.php",
+							'CORE_FOLDER' => CORE_FOLDER, 'CORE_PATH' => CORE_PATH,
+							'PLUGIN_PATH' => PLUGIN_PATH, 'PLUGIN_FOLDER' => PLUGIN_FOLDER,
+							'USER_PLUGIN_PATH' => USER_PLUGIN_PATH, 'USER_PLUGIN_FOLDER' => USER_PLUGIN_FOLDER,
+							'DATA_FOLDER' => DATA_FOLDER,
+							'CONFIGFILE' => CONFIGFILE,
+							'RW_SUFFIX' => preg_quote(getOption('mod_rewrite_suffix'))
 					);
+					$data = strtr($data, $defines);
 					break;
-				case 'closed_for_test':
-					$buttons[] = array(
-							'XSRFTag' => 'site_upgrade',
-							'category' => gettext('Admin'),
-							'enable' => 2,
-							'button_text' => gettext('Site » open'),
-							'formname' => 'site_upgrade',
-							'action' => getAdminLink(PLUGIN_FOLDER . '/site_upgrade/site_upgrade.php'),
-							'icon' => LOCK_OPEN,
-							'title' => gettext('Make site available for viewing.'),
-							'alt' => '',
-							'hidden' => '<input type="hidden" name="siteState" value="open" />',
-							'rights' => ADMIN_RIGHTS
-					);
-					list($diff, $needs) = checkSignature(0);
-					if (empty($needs)) {
-						?>
-						<script type="text/javascript">
-							window.addEventListener('load', function () {
-								$('#site_upgrade').submit(function () {
-									return confirm('<?php echo gettext('Your setup scripts are not protected!'); ?>');
-								})
-							}, false);
-						</script>
-						<?php
-					}
+				case '+':
+					$data = file_get_contents(PLUGIN_SERVERPATH . 'site_upgrade/' . $name);
+					$data = sprintf($data, sprintf(gettext('%s upgrade'), $_gallery->getTitle()), FULLWEBPATH . '/' . CORE_FOLDER . '/' . PLUGIN_FOLDER . '/site_upgrade/closed.png', sprintf(gettext('<strong><em>%s</em></strong> is undergoing an upgrade'), $_gallery->getTitle()), '<a href="' . FULLWEBPATH . '/index.php">' . gettext('Please return later') . '</a>', FULLWEBPATH . '/index.php');
 					break;
 				default:
-					$buttons[] = array(
-							'XSRFTag' => 'site_upgrade',
-							'category' => gettext('Admin'),
-							'enable' => true,
-							'button_text' => gettext('Site » close'),
-							'formname' => 'site_upgrade.php',
-							'action' => getAdminLink(PLUGIN_FOLDER . '/site_upgrade/site_upgrade.php'),
-							'icon' => LOCK,
-							'title' => gettext('Make site unavailable for viewing by redirecting to the "closed.html" page.'),
-							'alt' => '',
-							'hidden' => '<input type="hidden" name="siteState" value="closed" />',
-							'rights' => ADMIN_RIGHTS
+					// Feed plugin
+					$plugin = getPlugin(substr($name, 0, strpos($name, '-')) . '.php');
+					require_once( $plugin);
+					$items = array(
+							array(
+									'title' => sprintf(gettext('%s suspended'), $source),
+									'link' => FULLWEBPATH . '/index.php',
+									'enclosure' => '',
+									'category' => 'suspend',
+									'media_content' => '',
+									'media_thumbnail' => '',
+									'pubdate' => formattedDate("r", time()),
+									'desc' => sprintf(gettext('The %s feed is currently not available.'), $source)
+							)
 					);
+					$obj = new $source(array(strtolower($source) => 'null'));
+					ob_start();
+					$obj->printFeed($items);
+					$data = ob_get_clean();
 					break;
 			}
-
-			return $buttons;
+			file_put_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name, $data);
+			$hash[$name] = md5(file_get_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name));
 		}
-
-		static function updateXML($files) {
-			global $_gallery;
-			mkdir_recursive(USER_PLUGIN_SERVERPATH . 'site_upgrade/', FOLDER_MOD);
-			$hash = array();
-			foreach ($files as $name => $source) {
-				switch ($source) {
-					case '*':
-						$data = file_get_contents(PLUGIN_SERVERPATH . 'site_upgrade/' . $name);
-						$defines = array(
-								'SITEINDEX' => "/index.php",
-								'CORE_FOLDER' => CORE_FOLDER, 'CORE_PATH' => CORE_PATH,
-								'PLUGIN_PATH' => PLUGIN_PATH, 'PLUGIN_FOLDER' => PLUGIN_FOLDER,
-								'USER_PLUGIN_PATH' => USER_PLUGIN_PATH, 'USER_PLUGIN_FOLDER' => USER_PLUGIN_FOLDER,
-								'DATA_FOLDER' => DATA_FOLDER,
-								'CONFIGFILE' => CONFIGFILE,
-								'RW_SUFFIX' => preg_quote(getOption('mod_rewrite_suffix'))
-						);
-						$data = strtr($data, $defines);
-						break;
-					case '+':
-						$data = file_get_contents(PLUGIN_SERVERPATH . 'site_upgrade/' . $name);
-						$data = sprintf($data, sprintf(gettext('%s upgrade'), $_gallery->getTitle()), FULLWEBPATH . '/' . CORE_FOLDER . '/' . PLUGIN_FOLDER . '/site_upgrade/closed.png', sprintf(gettext('<strong><em>%s</em></strong> is undergoing an upgrade'), $_gallery->getTitle()), '<a href="' . FULLWEBPATH . '/index.php">' . gettext('Please return later') . '</a>', FULLWEBPATH . '/index.php');
-						break;
-					default:
-						// Feed plugin
-						$plugin = getPlugin(substr($name, 0, strpos($name, '-')) . '.php');
-						require_once( $plugin);
-						$items = array(
-								array(
-										'title' => sprintf(gettext('%s suspended'), $source),
-										'link' => FULLWEBPATH . '/index.php',
-										'enclosure' => '',
-										'category' => 'suspend',
-										'media_content' => '',
-										'media_thumbnail' => '',
-										'pubdate' => formattedDate("r", time()),
-										'desc' => sprintf(gettext('The %s feed is currently not available.'), $source)
-								)
-						);
-						$obj = new $source(array(strtolower($source) => 'null'));
-						ob_start();
-						$obj->printFeed($items);
-						$data = ob_get_clean();
-						break;
-				}
-				file_put_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name, $data);
-				$hash[$name] = md5(file_get_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name));
-			}
-			$prior = getSerializedArray(getOption('site_upgrade_hash'));
-			$hash = array_merge($prior, $hash);
-			setOption('site_upgrade_hash', serialize($hash));
-		}
-
-		static function replace($name) {
-			$prior = getSerializedArray(getOption('site_upgrade_hash'));
-			if (isset($prior[$name]) && file_exists(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name)) {
-				$hash = md5(file_get_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name));
-				return $hash = $prior[$name];
-			} else {
-				return true;
-			}
-		}
-
+		$prior = getSerializedArray(getOption('site_upgrade_hash'));
+		$hash = array_merge($prior, $hash);
+		setOption('site_upgrade_hash', serialize($hash));
 	}
 
-	switch (OFFSET_PATH) {
-		case 0:
-			$state = isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL;
-			if ((!npg_loggedin(ADMIN_RIGHTS | DEBUG_RIGHTS) && $state == 'closed_for_test') || $state == 'closed') {
-				header('location: ' . getAdminLink(USER_PLUGIN_FOLDER . '/site_upgrade/closed.php'));
-				exit();
-			} else if ($state == 'closed_for_test') {
-				npgFilters::register('theme_body_open', 'site_upgrade::notice');
-			}
-			break;
-		default:
-			npgFilters::register('admin_utilities_buttons', 'site_upgrade::button');
-			npgFilters::register('installation_information', 'site_upgrade::status');
-			npgFilters::register('admin_note', 'site_upgrade::note');
-
-			if (isset($_GET['refreshHTML'])) {
-				XSRFdefender('site_upgrade_refresh');
-				site_upgrade::updateXML(npgFilters::apply('site_upgrade_xml', SITE_UPGRADE_FILELIST));
-				$_GET['report'] = gettext('site_upgrade files Restored to original.');
-			}
-			break;
-		case 2:
-			foreach (SITE_UPGRADE_FILELIST as $file => $type) {
-				if (site_upgrade::replace($file)) {
-					site_upgrade::updateXML(array($file => $type));
-				}
-			}
-			break;
+	static function replace($name) {
+		$prior = getSerializedArray(getOption('site_upgrade_hash'));
+		if (isset($prior[$name]) && file_exists(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name)) {
+			$hash = md5(file_get_contents(USER_PLUGIN_SERVERPATH . 'site_upgrade/' . $name));
+			return $hash = $prior[$name];
+		} else {
+			return true;
+		}
 	}
+
+}
+
+switch (OFFSET_PATH) {
+	case 0:
+		$state = isset($_conf_vars['site_upgrade_state']) ? $_conf_vars['site_upgrade_state'] : NULL;
+		if ((!npg_loggedin(ADMIN_RIGHTS | DEBUG_RIGHTS) && $state == 'closed_for_test') || $state == 'closed') {
+			header('location: ' . getAdminLink(USER_PLUGIN_FOLDER . '/site_upgrade/closed.php'));
+			exit();
+		} else if ($state == 'closed_for_test') {
+			npgFilters::register('theme_body_open', 'site_upgrade::notice');
+		}
+		break;
+	default:
+		npgFilters::register('admin_utilities_buttons', 'site_upgrade::button');
+		npgFilters::register('installation_information', 'site_upgrade::status');
+		npgFilters::register('admin_note', 'site_upgrade::note');
+
+		if (isset($_GET['refreshHTML'])) {
+			XSRFdefender('site_upgrade_refresh');
+			site_upgrade::updateXML(npgFilters::apply('site_upgrade_xml', SITE_UPGRADE_FILELIST));
+			$_GET['report'] = gettext('site_upgrade files Restored to original.');
+		}
+		break;
+	case 2:
+		foreach (SITE_UPGRADE_FILELIST as $file => $type) {
+			if (site_upgrade::replace($file)) {
+				site_upgrade::updateXML(array($file => $type));
+			}
+		}
+		break;
 }
