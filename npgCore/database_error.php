bda54a02d87269117db8df0a8040f81214506a45
Stephen <stephen@sbillard.org>
2023-12-26 16:23:21 -0800

    dragable MySQL error division

--- npgCore/database_error.php
+++ npgCore/database_error.php
@@ -11,9 +11,7 @@ function displayQueryError($sql) {
 	if (defined('TESTING_MODE') && TESTING_MODE) {
 		trigger_error(sprintf(gettext('%1$s Error: ( %2$s ) failed. %1$s returned the error %3$s'), DATABASE_SOFTWARE, $sql, db_errorno() . ': ' . db_error()), E_USER_ERROR);
 	} else {
-
 		$reason = sprintf(gettext('%1$s Error %2$s'), DATABASE_SOFTWARE, db_errorno() . ': ' . db_error());
-
 		debugLogBacktrace(sprintf(gettext("Database Server error:\n %1\$s\n returned\n %2\$s."), $sql, $reason));
 	}
 	?>
@@ -41,11 +39,7 @@ function displayQueryError($sql) {
 			padding-right: 1em;
 			padding-top: 10px;
 			padding-bottom: 5px;
-			background-color: rgba(255,255,244,0.85);
-			border: 3px solid #CBCBCB;
-			position:fixed;
 			top: 50px;
-			left: 100px;
 			min-width: 640px;
 			z-index: 89999;
 		}
@@ -56,13 +50,95 @@ function displayQueryError($sql) {
 			font-weight: bold;
 			margin-bottom: 1em;
 		}
+
+		#dragbox {
+			position: absolute;
+			z-index: 9;
+			background-color: #f1f1f1;
+			border: 1px solid #d3d3d3;
+			text-align: center;
+			text-align: left;
+			padding-left: 1em;
+			padding-right: 1em;
+			padding-top: 10px;
+			padding-bottom: 5px;
+			background-color: rgba(255,255,244,0.85);
+			border: 3px solid #CBCBCB;
+			position: absolute;
+			top: 50px;
+			left: 100px;
+			min-width: 640px;
+			z-index: 89990;
+		}
+
+		#dragboxheader {
+			padding: 10px;
+			cursor: move;
+			z-index: 89995;
+			color: gray;
+		}
 	</style>
-	<br />
-	<div id="error_content">
-		<h1><?php echo gettext('Database Server Error'); ?></h1>
-		<div class="reasonbox">
-	<?php echo $reason; ?>
+
+	<!-- Draggable DIV -->
+	<br/>
+	<div id="dragbox">
+		<!-- Include a header DIV with the same name as the draggable DIV, followed by "header" -->
+		<div id="dragboxheader">Click here to move
+			<div id="error_content">
+				<h1><?php echo gettext('Database Server Error'); ?></h1>
+				<div class="reasonbox">
+					<?php echo $reason; ?>
+				</div>
+			</div>
 		</div>
 	</div>
+
+
+
+	<script type="text/javascript">
+		// Make the DIV element draggable:
+		dragElement(document.getElementById("mydiv"));
+
+		function dragElement(elmnt) {
+			var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+			if (document.getElementById(elmnt.id + "header")) {
+				// if present, the header is where you move the DIV from:
+				document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+			} else {
+				// otherwise, move the DIV from anywhere inside the DIV:
+				elmnt.onmousedown = dragMouseDown;
+			}
+
+			function dragMouseDown(e) {
+				e = e || window.event;
+				e.preventDefault();
+				// get the mouse cursor position at startup:
+				pos3 = e.clientX;
+				pos4 = e.clientY;
+				document.onmouseup = closeDragElement;
+				// call a function whenever the cursor moves:
+				document.onmousemove = elementDrag;
+			}
+
+			function elementDrag(e) {
+				e = e || window.event;
+				e.preventDefault();
+				// calculate the new cursor position:
+				pos1 = pos3 - e.clientX;
+				pos2 = pos4 - e.clientY;
+				pos3 = e.clientX;
+				pos4 = e.clientY;
+				// set the element's new position:
+				elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+				elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+			}
+
+			function closeDragElement() {
+				// stop moving when mouse button is released:
+				document.onmouseup = null;
+				document.onmousemove = null;
+			}
+		}
+	</script>
 	<?php
 }
