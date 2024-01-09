bebd32905cdc2d01b4f9075fbff857da10230e8e
Stephen <stephen@sbillard.org>
2024-01-09 14:47:32 -0800

    the require does not maintain the function context, so have to use class name instead of "self"

--- npgCore/extensions/deprecated-functions/snippets/getHashAlgorithm.php
+++ npgCore/extensions/deprecated-functions/snippets/getHashAlgorithm.php
@@ -5,10 +5,10 @@
  */
 
 $index = $userdata['passhash'];
-if (!array_search($index, self::$hashList)) { //	default
+if (!array_search($index, _Authority::$hashList)) { //	default
 	$index = PASSWORD_FUNCTION_DEFAULT;
 }
-$name = array_search($index, self::$hashList);
+$name = array_search($index, _Authority::$hashList);
 require_once(PLUGIN_SERVERPATH . 'deprecated-functions.php');
 if (!empty($userdata['pass'])) {
 	deprecated_functions::deprecationMessage(sprintf(gettext('The password for user %1$s is using the deprecated %2$s hashing method.'), $userdata['user'], $name));
