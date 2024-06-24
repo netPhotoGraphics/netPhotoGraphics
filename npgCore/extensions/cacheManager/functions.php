11be93584e247f03e0f69efa2c3f236f295938c8
Stephen <stephen@sbillard.org>
2024-06-24 13:02:49 -0700

    some reorganizstion

--- npgCore/extensions/cacheManager/functions.php
+++ npgCore/extensions/cacheManager/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @package plugins/cacheManager
  */
@@ -144,4 +143,135 @@ function updateCacheName($text, $target, $update) {
 	return $text;
 }
 
-?>
+function loadAlbum($album) {
+	global $_current_album, $_current_image, $_gallery, $custom, $enabled, $fullImage;
+	$subalbums = $album->getAlbums();
+	sort($subalbums);
+	$started = false;
+	$tcount = $count = 0;
+	foreach ($subalbums as $folder) {
+		$subalbum = newAlbum($folder);
+		if (!$subalbum->isDynamic()) {
+			$tcount = $tcount + loadAlbum($subalbum);
+		}
+	}
+	$theme = $_gallery->getCurrentTheme();
+	$id = 0;
+	$parent = $album->getUrAlbum();
+	$albumtheme = $parent->getAlbumTheme();
+	if (!empty($albumtheme)) {
+		$theme = $albumtheme;
+		$id = $parent->getID();
+	}
+	loadLocalOptions($id, $theme);
+	$_current_album = $album;
+	if ($album->getNumImages() > 0) {
+		$needsCaching = array();
+
+		echo "<br />" . $album->name . ' ';
+		while (next_image(true)) {
+			if ($_current_image->isPhoto()) {
+
+				if ($fullImage) {
+					$uri = getFullImageURL(NULL, 'Protected');
+					if (strpos($uri, 'full-image.php?') !== false) {
+						$needsCaching[] = array(
+								'uri' => preg_replace('~/i\.(.*)\?~', '/i.php?', $uri),
+								'cache' => ltrim(getImageCacheFilename($_current_image->album->name, $_current_image->filename, $args), '/')
+						);
+					}
+				}
+
+				foreach ($custom as $key => $cacheimage) {
+					if (in_array($key, $enabled)) {
+						$size = isset($cacheimage['image_size']) ? $cacheimage['image_size'] : NULL;
+						$width = isset($cacheimage['image_width']) ? $cacheimage['image_width'] : NULL;
+						$height = isset($cacheimage['image_height']) ? $cacheimage['image_height'] : NULL;
+						$thumb = isset($cacheimage['thumb']) ? $cacheimage['thumb'] : NULL;
+						if ($special = ($thumb === true)) {
+							list($special, $cw, $ch, $cx, $cy) = $_current_image->getThumbCropping($size, $width, $height);
+						}
+						if (!$special) {
+							$cw = isset($cacheimage['crop_width']) ? $cacheimage['crop_width'] : NULL;
+							$ch = isset($cacheimage['crop_height']) ? $cacheimage['crop_height'] : NULL;
+							$cx = isset($cacheimage['crop_x']) ? $cacheimage['crop_x'] : NULL;
+							$cy = isset($cacheimage['crop_y']) ? $cacheimage['crop_y'] : NULL;
+						}
+						$effects = isset($cacheimage['gray']) ? $cacheimage['gray'] : NULL;
+						if (isset($cacheimage['wmk'])) {
+							$WM = $cacheimage['wmk'];
+						} else if ($cacheimage['thumb'] < 0) {
+							$WM = '!';
+						} else if ($thumb) {
+							$WM = getWatermarkParam($_current_image, WATERMARK_THUMB);
+						} else {
+							$WM = getWatermarkParam($_current_image, WATERMARK_IMAGE);
+						}
+						if (isset($cacheimage['maxspace'])) {
+							getMaxSpaceContainer($width, $height, $_current_image, $thumb);
+						}
+						$args = array('size' => $size, 'width' => $width, 'height' => $height, 'cw' => $cw, 'ch' => $ch, 'cx' => $cx, 'cy' => $cy, 'thumb' => $thumb, 'WM' => $WM, 'effects' => $effects);
+						$args = getImageParameters($args, $album->name);
+						$uri = getImageURI($args, $_current_image->album->name, $_current_image->filename, $_current_image->filemtime);
+						if (strpos($uri, '/' . CORE_FOLDER . '/i.') !== false) {
+							$needsCaching[] = array(
+									'uri' => preg_replace('~/i\.(.*)\?~', '/i.php?', $uri),
+									'cache' => ltrim(getImageCacheFilename($_current_image->album->name, $_current_image->filename, $args), '/')
+							);
+						}
+					}
+				}
+			}
+		}
+		$count = count($needsCaching);
+		if ($count) {
+			echo '{ ';
+			if (CURL_ENABLED) {
+				$sections = array_chunk($needsCaching, min(5, THREAD_CONCURRENCY - 1), true);
+				foreach ($sections as $block) {
+					set_time_limit(200);
+					$uriList = array();
+					foreach ($block as $key => $img) {
+						$uriList[$key] = FULLHOSTPATH . preg_replace('~\&cached=\d+\&~', '&', $img['uri']) . '&returncheckmark&curl=' . sha1(CORE_SERVERPATH);
+					}
+					$checks = new ParallelCURL($uriList);
+					$rsp = $checks->getResults();
+					foreach ($block as $key => $img) {
+						if ($key) {
+							echo ' | ';
+						}
+						if (isset($rsp[$key]) && is_numeric($rsp[$key])) {
+							?>
+							<img src = "<?php echo FULLWEBPATH . '/' . CORE_FOLDER . '/setup/icon.php?icon=' . ($rsp[$key] - 1); ?>" title="<?php echo html_encode($img['cache']); ?>" height = "16px" width = "16px">
+							<?php
+						} else {
+							?>
+							<a href="<?php echo $img['uri'] . '&amp;debug'; ?>" target="_blank" title="<?php echo html_encode($img['cache']); ?>"><?php echo CROSS_MARK_RED; ?></a>
+							<?php
+						}
+					}
+					if (ob_get_length()) {
+						ob_flush();
+					}
+					flush();
+				}
+			} else {
+				set_time_limit(200);
+				foreach ($needsCaching as $key => $img) {
+					if ($key) {
+						echo ' | ';
+					}
+					?>
+					<a href="<?php echo $img['uri']; ?>&amp;admin&amp;returncheckmark&amp;debug">
+						<?php echo '<img src="' . $img['uri'] . '&amp;returncheckmark" title="' . html_encode($img['cache']) . '" height="16" width="16" alt="X" />' . "\n"; ?>
+					</a>
+					<?php
+				}
+			}
+			echo '} ';
+		}
+		printf(ngettext('[%u image]', '[%u images]', $count), $count);
+		echo "<br />\n";
+	}
+	return $count + $tcount;
+}
