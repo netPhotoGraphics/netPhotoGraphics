21da06c0e66c57213dcc6e56bdec0f6001a9faaf
Stephen Billard <stephen@sbillard.org>
2025-10-11 12:48:52 -0700

    Optional use of mutex threading

--- npgCore/class-mutex.php
+++ npgCore/class-mutex.php
@@ -97,6 +97,7 @@ class npgMutex {
 		if ($this->locked) {
 			//Only unlock a locked mutex.
 			$this->locked = false;
+			fwrite($this->mutex, "Unlocked \n");
 			flock($this->mutex, LOCK_UN);
 			fclose($this->mutex);
 			return true;
