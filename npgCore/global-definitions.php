f4444ea157428bb2c0acb354b2d461f48bc5a1ce
Stephen Billard <stephen@sbillard.org>
2025-10-16 16:36:51 -0700

    revert min PHP version to 7.4 to allow for transition

--- npgCore/global-definitions.php
+++ npgCore/global-definitions.php
@@ -2,7 +2,7 @@
 
 const stdExclude = array('Thumbs.db', 'readme.md', 'data', '.', '..');
 
-Define('PHP_MIN_VERSION', '8.0');
+Define('PHP_MIN_VERSION', '7.4');
 Define('PHP_MIN_SUPPORTED_VERSION', '8.0');
 
 $time = time();
