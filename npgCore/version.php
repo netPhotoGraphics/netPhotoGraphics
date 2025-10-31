878862f49210cf59487825589c782e79bebf97a6
Stephen Billard <stephen@sbillard.org>
2025-10-30 18:12:27 -0700

    release build 2.01.05.01.28

--- npgCore/version.php
+++ npgCore/version.php
@@ -1,4 +1,4 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('NETPHOTOGRAPHICS_VERSION', '2.01.05.01.27');
+define('NETPHOTOGRAPHICS_VERSION', '2.01.05.01.28');
 ?>
