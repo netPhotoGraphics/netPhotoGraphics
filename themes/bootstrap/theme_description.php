dab3b7c5a470d56f07ecc4922fa9b9a29f29411d
MartinKroul <martin.kroul@skaut.cz>
2026-06-12 22:34:33 +0200

    Update theme_description.php
    Correted two typos

--- themes/bootstrap/theme_description.php
+++ themes/bootstrap/theme_description.php
@@ -7,8 +7,8 @@ $theme_description['name'] = 'Bootstrap';
 $theme_description['author'] = 'Vincent3569';
 $theme_description['version'] = '2.3';
 $theme_description['date'] = '31/03/2020';
-$theme_description['desc'] = gettext('A responsive theme ready for desktop to mobile device, using <a href="https://getbootstrap.com/docs/3.4/" title="Boostrap" target="_blank">Boostrap 3.4.0</a> framework.') . '<br />' .
+$theme_description['desc'] = gettext('A responsive theme ready for desktop to mobile device, using <a href="https://getbootstrap.com/docs/3.4/" title="Bootstrap" target="_blank">Boostrap 3.4.0</a> framework.') . '<br />' .
 				gettext('Please read') . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/readme.htm' . '" title="Readme File" target="_blank">Readme</a>, <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/license.htm' . '" title="License File" target="_blank">License</a> ' .
 				gettext('and') . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/changelog.txt' . '" title="ChangeLog File" target="_blank">ChangeLog</a><br />' .
-				gettext('To design your content to be responsive using Boostrap framework, you can have a look on') . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/bootstrap_grid_system.htm' . '" title="Bootstrap Grid System" target="_blank">' .
+				gettext('To design your content to be responsive using Bootstrap framework, you can have a look on') . ' <a href="' . FULLWEBPATH . '/' . THEMEFOLDER . '/' . $me . '/bootstrap_grid_system.htm' . '" title="Bootstrap Grid System" target="_blank">' .
 				gettext('Bootstrap Grid System') . '</a>';
