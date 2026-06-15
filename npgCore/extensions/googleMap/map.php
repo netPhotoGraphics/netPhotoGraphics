a4b170008a99168252e1169fa71caf48da0499a0
Stephen Billard <stephen@sbillard.org>
2026-06-14 18:01:53 -0700

    duplicated code

--- npgCore/extensions/googleMap/map.php
+++ npgCore/extensions/googleMap/map.php
@@ -13,8 +13,6 @@ if (getOption('gmap_sessions')) {
 	npg_session_start();
 }
 require_once(dirname(__DIR__) . '/googleMap.php');
-
-GoogleMap::js();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/2002/REC-xhtml1-20020801/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" />
